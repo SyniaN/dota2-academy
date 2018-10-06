e6875306ee2e461a853d947c6ff82635b6e0cd31
David Xu <david.xu@spark.co.nz>
2018-10-06 18:16:55 +1300

    fixed a bug

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -5,7 +5,7 @@ export default class FlashCard extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="title">{this.props.question}</h1>
+        <h1 className="title">{this.props.question.text}</h1>
       </div>
     );
   }
