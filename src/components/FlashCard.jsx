1c70da10e668c35025affa8a9fee0bbd15aaea8f
sumiawang <sumia.wang@gmail.com>
2018-10-06 19:27:30 +1300

    Added radio buttons

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -6,6 +6,25 @@ export default class FlashCard extends React.Component {
     return (
       <div>
         <h1 className="title">{this.props.question.text}</h1>
+
+     <div>
+       <input type="radio" id="123"
+       name="potentialAnswers" value="123"/>
+      <label for="123">123</label>
+     </div>
+
+     <div>
+       <input type="radio" id="456"
+       name="potentialAnswers" value="456"/>
+      <label for="123">456</label>
+     </div>
+
+     <div>
+       <input type="radio" id="789"
+       name="potentialAnswers" value="789"/>
+      <label for="123">789</label>
+     </div>
+
       </div>
     );
   }
