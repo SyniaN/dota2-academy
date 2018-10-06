5074c643cc43cf5a391981e81f84e1e8a5a85f9b
sumiawang <sumia.wang@gmail.com>
2018-10-06 21:18:49 +1300

    Generate html based on passed-in potential answers array

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -7,24 +7,22 @@ export default class FlashCard extends React.Component {
       <div>
         <h1 className="title">{this.props.question.text}</h1>
 
-     <div>
-       <input type="radio" id="123"
-       name="potentialAnswers" value="123"/>
-      <label for="123">123</label>
-     </div>
-
-     <div>
-       <input type="radio" id="456"
-       name="potentialAnswers" value="456"/>
-      <label for="123">456</label>
-     </div>
-
-     <div>
-       <input type="radio" id="789"
-       name="potentialAnswers" value="789"/>
-      <label for="123">789</label>
-     </div>
-
+        {this.props.potentialAnswers.map((answer, index) => {
+          return (
+            <div>
+              <input
+                onClick={() => {
+                  this.props.onAnswerSelect(index);
+                }}
+                type="radio"
+                name="answer"
+                value={index}
+                checked={this.props.selectedAnswerIndex === index}
+              />
+              <label for={index}>{answer.text}</label>
+            </div>
+          );
+        })}
       </div>
     );
   }
